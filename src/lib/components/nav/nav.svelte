60fd67d59293b356f232bde2084fb97a9cdac904
AbdullahMRiad <abdullahmriad@gmail.com>
2026-07-22 18:04:50 +0300

    show user info in nav if the user is logged in

--- src/lib/components/nav/nav.svelte
+++ src/lib/components/nav/nav.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
     import icon from '$lib/assets/favicon.svg';
+    import { resolve } from '$app/paths'
+    import type { User } from '@supabase/supabase-js';
+    import type { LayoutData } from '../../../routes/$types';
+    import { onMount } from 'svelte';
+
+    let { data }: { data: LayoutData } = $props();
+    let user: User | null = $state(null);
+
+    onMount(async () => {
+        const { data: { user: fetchedUser } } = await data.supabase.auth.getUser();
+        user = fetchedUser;
+    });
 </script>
 
 <nav>
@@ -10,6 +22,18 @@
         <li><a href="#!">عنا</a></li>
         <li><a href="#!">الآراء</a></li>
         <li><a href="#!">الأقسام</a></li>
+        <li class="account">
+            {#if user}
+               {`مرحبًا ${user.user_metadata.first_name ?? user.email?.split("@")[0]}`}
+                <button class="btn btn-ghost" onclick={()=>{data.supabase.auth.signOut({scope: 'local'})}}>تسجيل الخروج</button>
+            {:else}
+                <a href={resolve('/login')}>
+                    تسجيل الدخول
+                </a>
+                أو
+                <a class="btn btn-primary" href={resolve('/signup')}>إنشاء حساب</a>
+            {/if}
+        </li>
     </ul>
 </nav>
 
@@ -53,6 +77,14 @@
             margin-inline: var(--spacing-6);
             box-shadow: 0 0.75rem 2rem light-dark(hsl(0 0 0 / 0.15), hsl(0 0 0 / 0.25));
         }
+    }
+
+    .account {
+        margin-inline-start: auto;
+        display: flex;
+        flex-direction: row;
+        gap: 0.75rem;
+        align-items: center;
     }
 
     @keyframes stick-to-top {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
     import Nav from '$lib/components/nav/nav.svelte';
     import Footer from '$lib/components/footer/footer.svelte';
 
-    let { children } = $props();
+    let { children, data } = $props();
 </script>
 
 <svelte:head>
@@ -15,6 +15,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
-<Nav />
+<Nav data={data} />
 {@render children()}
 <Footer />
