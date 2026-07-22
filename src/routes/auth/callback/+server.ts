220cb62480318cb8b095aed2c45bbd9a6274720c
AbdullahMRiad <abdullahmriad@gmail.com>
2026-07-22 16:28:57 +0300

    add oauth callback route

---
+++ src/routes/auth/callback/+server.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
+export const GET = async ({ url, locals }) => {
+  const code = url.searchParams.get('code');
+  const next = url.searchParams.get('next') ?? '/';
+
+  if (code) {
+    const { error } = await locals.supabase.auth.exchangeCodeForSession(code);
+    if (!error) throw redirect(303, next);
+  }
+
+  throw redirect(303, '/login');
+};
